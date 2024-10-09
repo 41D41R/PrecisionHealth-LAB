e0fdbab992c94ff6f741d99c311fbcac1be9807b
Aldair Hernández-Morales <78239708+41D41R@users.noreply.github.com>
2024-10-08 18:27:28 -0600

    I changed the read of the input file IFS from , to tab

--- Pipeline_Dementia_UPDATE/Codes/sacbe_individual.sh
+++ Pipeline_Dementia_UPDATE/Codes/sacbe_individual.sh
@@ -5,7 +5,7 @@ archivo="Genes.csv"
 # Obtener el encabezado fuera del bucle
 encabezado=$(awk NR==1 G6PD_coordinates_sacbe.varfile.ex.EDITED)
 # Itera a través de cada línea en el archivo (omitir la primera línea con sed)
-tail -n +2 "$archivo" | while IFS=',' read -r GENE CHR START END; do
+tail -n +2 "$archivo" | while IFS=$'\t' read -r GENE CHR START END; do
     chrom=$(echo "$CHR" | tr -cd '[:alnum:]')
     archivo_entrada="/mnt/Timina/cgonzaga/ahernandezm/g6pd/sacbe_genes/G6PD_coordinates_sacbe.varfile.ex.EDITED"
     archivo_salida="/mnt/Timina/cgonzaga/ahernandezm/g6pd/sacbe_genes/genes/${GENE}_sacbe.tsv"
